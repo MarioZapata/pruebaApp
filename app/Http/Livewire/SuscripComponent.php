f8728a387f4e2fb90163779edd2914701da5d389
MarioZ <github@uml.edu.ni>
2021-09-30 13:26:41 -0600

    Mensaje de Inicio

---
+++ app/Http/Livewire/SuscripComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class SuscripComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.suscrip-component');
+    }
+}
